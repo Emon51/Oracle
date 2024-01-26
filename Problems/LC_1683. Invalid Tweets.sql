f749c02482018c9907bb1551ddac2574f3b640ba
Emon51 <94897729+Emon51@users.noreply.github.com>
2024-01-26 12:16:29 +0600

    Update LC_1683. Invalid Tweets.sql

--- Problems/LC_1683. Invalid Tweets.sql
+++ Problems/LC_1683. Invalid Tweets.sql
@@ -3,3 +3,13 @@
 select t.tweet_id
 from Tweets t
 where length(t.content) > 15;
+----------------------------------------------
+
+CREATE PROCEDURE FindInvalidTweets AS
+BEGIN
+  SELECT tweet_id
+  FROM Tweets
+  WHERE LENGTH(content) > 15;
+END;
+
+EXEC FindInvalidTweets;
