04562cd42ea31a9fd2950dfdba1650e5587fff80
Emon51 <94897729+Emon51@users.noreply.github.com>
2024-01-28 12:07:43 +0600

    Create Problem_5.sql

---
+++ Problems/Problem_5.sql
@@ -0,0 +1,102 @@
+
+
+/* Write an SQL query to display the correct message (meaningful message) from the input
+comments_and_translation table. */
+
+CREATE TABLE comments_and_translations
+(
+    comment_id      INT,
+    comments         VARCHAR2(100),
+    translations     VARCHAR2(100)
+);
+
+
+--------------------------------------------------------------------------------
+insert into comments_and_translations values (1, 'very good', null);
+insert into comments_and_translations values (2, 'good', null);
+insert into comments_and_translations values (3, 'bad', null);
+insert into comments_and_translations values (4, 'ordinary', null);
+insert into comments_and_translations values (5, 'cdcdcdcd', 'very bad');
+insert into comments_and_translations values (6, 'excellent', null);
+insert into comments_and_translations values (7, 'ababab', 'not satisfied');
+insert into comments_and_translations values (8, 'satisfied', null);
+insert into comments_and_translations values (9, 'aabbaabb', 'extraordinary');
+insert into comments_and_translations values (10, 'ccddccbb', 'medium');
+--------------------------------------------------------------------------------
+
+ALTER TABLE comments_and_translations
+RENAME TO temp;
+--------------------------------------------------------------------------------
+SELECT * FROM temp;
+
+
+/* Write an SQL query to display the correct message (meaningful message) from the input
+comments_and_translation table. */
+
+CREATE TABLE comments_and_translations
+(
+    comment_id      INT,
+    comments         VARCHAR2(100),
+    translations     VARCHAR2(100)
+);
+
+
+--------------------------------------------------------------------------------
+insert into comments_and_translations values (1, 'very good', null);
+insert into comments_and_translations values (2, 'good', null);
+insert into comments_and_translations values (3, 'bad', null);
+insert into comments_and_translations values (4, 'ordinary', null);
+insert into comments_and_translations values (5, 'cdcdcdcd', 'very bad');
+insert into comments_and_translations values (6, 'excellent', null);
+insert into comments_and_translations values (7, 'ababab', 'not satisfied');
+insert into comments_and_translations values (8, 'satisfied', null);
+insert into comments_and_translations values (9, 'aabbaabb', 'extraordinary');
+insert into comments_and_translations values (10, 'ccddccbb', 'medium');
+--------------------------------------------------------------------------------
+
+ALTER TABLE comments_and_translations
+RENAME TO temp;
+--------------------------------------------------------------------------------
+SELECT * FROM temp;
+
+--Solution
+
+SELECT CASE WHEN translations IS NULL THEN comments ELSE translations END AS output
+FROM temp;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
