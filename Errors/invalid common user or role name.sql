445a2c597474e12927411fc037c16b988f4631a5
Emon51 <94897729+Emon51@users.noreply.github.com>
2024-02-20 00:09:29 +0600

    Update invalid common user or role name.sql

--- Errors/invalid common user or role name.sql
+++ Errors/invalid common user or role name.sql
@@ -1,3 +1,4 @@
 
 --while creating new user using sql plus sometimes occurs a error: ORA-00922: missing or invalid option
 --Solution:  ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+--And try to create new user --- CREATE [new_user_name] IDENTIFIED BY [new_password];
