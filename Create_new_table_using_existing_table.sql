3d99cfdf95c55a95f8b792176c192b2404795e07
Emon51 <94897729+Emon51@users.noreply.github.com>
2024-02-19 10:41:09 +0600

    Update Create_new_table_using_existing_table.sql

--- Create_new_table_using_existing_table.sql
+++ Create_new_table_using_existing_table.sql
@@ -1,1 +1,20 @@
 
+--Syntax
+
+--Create new table or backup table using existing table
+
+CREATE TABLE [new_table_name] AS (SELECT * FROM [old_table_name])    --Parenthesis not mandatory
+
+
+--Create new table structure without data using existing table
+
+CREATE TABLE [new_table_name] AS (SELECT * FROM [old_table_name])    --Parenthesis not mandatory
+WHERE 1 = 2 --False Condition
+
+--Copy all records or specific records(using where clause associated with specified conditions).
+
+INSERT INTO [new_table_name] (SELECT * FROM [old_table_name]) --Parenthesis not mandatory
+
+--OR
+
+INSERT INTO [new_table_name] (SELECT * FROM [old_table_name] WHERE [conditions]) --Parenthesis not mandatory
